9320a8f5cc48ca87f5c17c752533029a7bae78d7
Isha Negi <itsishanegi@gmail.com>
2024-11-24 12:36:26 +0530

    Create area_of_circle.c

---
+++ area_of_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float radius,areaOfCircle;
+    printf("Enter radius: ");
+    scanf("%f",&radius);
+    areaOfCircle=M_PI*radius*radius;
+    printf("Area of circle= %f",areaOfCircle);
+    printf("\nName: Isha Negi \nRoll number: 24 CE 47\n");
+}
